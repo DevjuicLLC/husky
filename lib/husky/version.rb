4c3b30be20919641552e0402b20641330fbc5eb7
Stephen Fiser <stephen.fiser@gmail.com>
2016-02-14 10:44:52 -0500

    first commit

---
+++ lib/husky/version.rb
@@ -0,0 +1,3 @@
+module Husky
+  VERSION = "0.0.1"
+end
---
+++ test/husky_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HuskyTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, Husky
+  end
+end
