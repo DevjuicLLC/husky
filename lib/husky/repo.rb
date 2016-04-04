f37f17a825a087fd1352b28aac5c175cd63d8486
Stephen Fiser <stephen.fiser@gmail.com>
2016-04-04 07:45:27 -0400

    makes repo methods instance instead of class level

--- lib/husky/repo.rb
+++ lib/husky/repo.rb
@@ -1,47 +1,50 @@
 module Husky
+
   class Repo
 
-    class << self
+    def new(attributes = {})
+      entity.new(data_source.new(attributes))
+    end
 
-      def find(id)
-        entity.new(data_source.find(id))
-      end
+    def find(id)
+      entity.new(data_source.find(id))
+    end
 
-      def all
-        entity.wrap(data_source.all)
-      end
+    def all
+      entity.wrap(data_source.all)
+    end
 
-      def build(attributes)
-        entity.new(data_source.build(attributes))
-      end
+    # DEPRECATE
+    def build(attributes)
+      entity.new(data_source.build(attributes))
+    end
 
-      def create(attributes)
-        entity.new(data_source.create(attributes))
-      end
+    def create(attributes)
+      entity.new(data_source.create(attributes))
+    end
 
-      def update(item, attributes)
-        entity.new(item.update_attributes(attributes))
-      end
+    def update(item, attributes)
+      entity.new(item.update_attributes(attributes))
+    end
 
-      def save(item)
-        item.save
-      end
+    def save(item)
+      item.save
+    end
 
-      def destroy(item)
-        item.destroy
-      end
+    def destroy(item)
+      item.destroy
+    end
 
-      private
+    private
 
-      def data_source
-        raise NotImplementedError
-      end
+    def data_source
+      raise NotImplementedError
+    end
 
-      def entity
-        raise NotImplementedError
-      end
-
+    def entity
+      raise NotImplementedError
     end
 
   end
+
 end
