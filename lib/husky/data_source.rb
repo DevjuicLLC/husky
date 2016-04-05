1062ee1dea66fd018b401330e692364707bc7dc8
Stephen Fiser <stephen.fiser@gmail.com>
2016-04-05 16:56:02 -0400

    adds registry

--- lib/husky/data_source.rb
+++ lib/husky/data_source.rb
@@ -10,7 +10,7 @@ module Husky
           define_method association do
             singular_association = association.to_s[0..-2].to_sym
             klass_id = "#{self.class.name.split('::')[1].downcase}_id".to_sym
-            Repository.for(singular_association).all.select { |k,v| v.send(klass_id) == self.id }
+            Husky::Repo::Registry.for(singular_association).all.select { |k,v| v.send(klass_id) == self.id }
           end
         end
 
--- lib/husky/version.rb
+++ lib/husky/version.rb
@@ -1,3 +1,3 @@
 module Husky
-  VERSION = "0.1.9"
+  VERSION = "0.2.0"
 end
