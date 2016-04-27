a041fc73acf9e735806ca71290290cf70acb1fcf
Stephen Fiser <stephen.fiser@gmail.com>
2016-04-26 21:41:25 -0400

    adds new initialize method on entity for hash

--- lib/husky/entity.rb
+++ lib/husky/entity.rb
@@ -1,29 +1,47 @@
-require 'forwardable'
+require 'delegate'
 
 module Husky
 
-  class Entity
-    extend Forwardable
+  class Entity < SimpleDelegator
 
     class << self
-
-      def fields(*field_names)
-        field_names.each do |field_name|
-          def_delegators :_data, field_name
-          def_delegators :_data, "#{field_name}="
-        end
-      end
 
       def wrap(items)
         items.map { |item| new(item) }
       end
 
+      def new_from_hash(hash)
+        raise hash_error(hash) unless hash.is_a? Hash
+        instance = allocate
+        instance.initialize_from_hash(hash)
+        instance
+      end
+
+      private
+
+      def hash_error(hash)
+        "Entity#initialize_from_hash expects a hash as an argument, and you provided a #{hash.class.name} (#=> #{hash})."
+      end
+
     end
 
-    attr_reader :_data
+    attr_reader :object
 
-    def initialize(_data)
-      @_data = _data
+    def initialize(object)
+      @object = object
+      super
+    end
+
+    def initialize_from_hash(post_data)
+      post_data.each_pair do |key, value|
+        instance_variable_set("@#{key}", value)
+      end
+
+      post_data.each_pair do |key, value|
+        self.class.send(:define_method, key) do
+          instance_variable_get("@#{key}")
+        end
+      end
     end
 
   end
